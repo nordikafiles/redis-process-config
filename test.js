198466f4f27966fbeac78786c2a403ca19f28b20
sadfsdf <mamogaaa@yandex.ru>
2020-10-27 12:12:39 +0300

    fixes

--- test.js
+++ test.js
@@ -6,30 +6,6 @@ const keyPrefix = "test_7vUUMQTrON";
 
 let manager = new RedisProcessManager({}, { keyPrefix });
 
-// async function contentFunc () {
-//     // await process.takeConfig()
-//     // console.log({
-//     //     id: process.id,
-//     //     config: process.config
-//     // })
-//     let configs = await manager.listConfigs()
-//     console.log({ configs })
-//     await manager.setConfig('alalaher', { blabla: 'tralala' })
-//     configs = await manager.listConfigs()
-//     console.log({ configs })
-//     await sleep(3000)
-//     try {
-
-//         await manager.removeConfig('alalaher')
-//     } catch (err) {
-//         console.log(err.toString())
-//         await manager.removeConfig('alalaher', true)
-//     }
-//     configs = await manager.listConfigs()
-//     console.log({ configs })
-// }
-// contentFunc()
-
 const assert = require("assert");
 
 it("should work", async () => {
